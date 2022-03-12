e1ce55b1213af148a8299109b2141e3c43f1b4a8
Dusan Nikolov <nikolodus@gmail.com>
2022-03-12 21:52:33 +0100

    fix: revert previous commit, issue with memory management

--- libsndfile_test_vs/test_OLA/src/main.cpp
+++ libsndfile_test_vs/test_OLA/src/main.cpp
@@ -22,17 +22,24 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    auto in = std::make_unique<SndfileHandle>(argv[1]);
+    SndfileHandle *in, *ir, *out;
+
+    in = new SndfileHandle(argv[1]);
     if (in->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "Input file not recognized!" << std::endl;
+
+        delete in;
         return -1;
     }
 
-    auto ir = std::make_unique<SndfileHandle>(argv[2]);
+    ir = new SndfileHandle(argv[2]);
     if (ir->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "IR file not recognized!" << std::endl;
+
+        delete in;
+        delete ir;
         return -1;
     }
 
@@ -42,14 +49,18 @@ int main(int argc, char **argv)
     strncpy(out_fn, argv[1], sizeof(char) * (in_fn_len - 4));
     strcpy(out_fn + in_fn_len - 4, "_out.wav");
 
-    auto out = std::make_unique<SndfileHandle>(out_fn,
-                                               SFM_WRITE,
-                                               SF_FORMAT_WAV | SF_FORMAT_PCM_16,
-                                               in->channels(),
-                                               44100);
+    out = new SndfileHandle(out_fn,
+                            SFM_WRITE,
+                            SF_FORMAT_WAV | SF_FORMAT_PCM_16,
+                            in->channels(),
+                            44100);
     if (out->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "Output file not formed!" << std::endl;
+
+        delete in;
+        delete ir;
+        delete out;
         return -1;
     }
 
@@ -57,10 +68,14 @@ int main(int argc, char **argv)
 
     std::cout << "::::: " << argv[1] << " : " << argv[2] << " :::::" << std::endl;
 
-    auto effect = std::make_unique<ReverbEffect>(in.get(), ir.get(), out.get());
+    ReverbEffect *effect = new ReverbEffect(in, ir, out);
 
     effect->applyReverb();
     effect->writeOutNormalized();
+
+    std::cout << std::endl;
+
+    delete effect;
 
     return 0;
 }
