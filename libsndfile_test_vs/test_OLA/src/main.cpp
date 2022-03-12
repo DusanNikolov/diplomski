8d379db5d8dfc4f6d7bc28a3a7b2afec78efefce
Dusan Nikolov <nikolodus@gmail.com>
2022-03-12 21:34:26 +0100

    refactor: use unique_ptr instead of RAW pointes

--- libsndfile_test_vs/test_OLA/src/main.cpp
+++ libsndfile_test_vs/test_OLA/src/main.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include <memory>
 
 int main(int argc, char **argv)
 {
@@ -21,24 +22,17 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    SndfileHandle *in, *ir, *out;
-
-    in = new SndfileHandle(argv[1]);
+    auto in = std::make_unique<SndfileHandle>(argv[1]);
     if (in->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "Input file not recognized!" << std::endl;
-
-        delete in;
         return -1;
     }
 
-    ir = new SndfileHandle(argv[2]);
+    auto ir = std::make_unique<SndfileHandle>(argv[2]);
     if (ir->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "IR file not recognized!" << std::endl;
-
-        delete in;
-        delete ir;
         return -1;
     }
 
@@ -48,18 +42,14 @@ int main(int argc, char **argv)
     strncpy(out_fn, argv[1], sizeof(char) * (in_fn_len - 4));
     strcpy(out_fn + in_fn_len - 4, "_out.wav");
 
-    out = new SndfileHandle(out_fn,
-                            SFM_WRITE,
-                            SF_FORMAT_WAV | SF_FORMAT_PCM_16,
-                            in->channels(),
-                            44100);
+    auto out = std::make_unique<SndfileHandle>(out_fn,
+                                               SFM_WRITE,
+                                               SF_FORMAT_WAV | SF_FORMAT_PCM_16,
+                                               in->channels(),
+                                               44100);
     if (out->error() != SF_ERR_NO_ERROR)
     {
         std::cerr << "Output file not formed!" << std::endl;
-
-        delete in;
-        delete ir;
-        delete out;
         return -1;
     }
 
@@ -67,14 +57,10 @@ int main(int argc, char **argv)
 
     std::cout << "::::: " << argv[1] << " : " << argv[2] << " :::::" << std::endl;
 
-    ReverbEffect *effect = new ReverbEffect(in, ir, out);
+    auto effect = std::make_unique<ReverbEffect>(in.get(), ir.get(), out.get());
 
     effect->applyReverb();
     effect->writeOutNormalized();
-
-    std::cout << std::endl;
-
-    delete effect;
 
     return 0;
 }
